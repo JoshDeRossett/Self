c05d3461ff3066ff82368e7ec2bb59f4857e05e0
JoshDeRossett <joshderossett1@gmail.com>
2016-04-10 15:26:45 -0400

    Full Project File

---
+++ Assets/_Scripts/BranchLoad.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BranchLoad : MonoBehaviour {
+    [SerializeField]
+    private GameObject start;
+    private void Start() { start.SetActive(true); }
+}
---
+++ Assets/_Scripts/NextBranch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextBranch : MonoBehaviour {
+    private _Globals.Call call = new _Globals.Call();
+    private GameObject warning;
+    private void Awake() {
+        warning = GameObject.FindGameObjectWithTag("Warning");
+        warning.SetActive(!call.proceed);
+    }
+    public void Advance() {
+        if (!call.proceed) return;
+        _Globals.branchesCompleted++;
+        call.DetermineCurrentIndex();
+        call.UpdateCompletedBranches();
+        call.GenerateNextBranch();
+        call.ErrorCheck();
+        warning.SetActive(!call.proceed);
+        if (call.proceed) SceneManager.LoadScene(_Globals.nextBranch);
+    }
+}
